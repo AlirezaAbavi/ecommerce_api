c7f7019b1a4841fd8c807d4d13f5a01424e54a5f
AlirezaAbavi <abavi.alireza2002@gmail.com>
2022-12-26 00:55:03 +0330

    configure app_account admin

--- app_account/admin.py
+++ app_account/admin.py
@@ -1,3 +1,54 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import (
+    User,
+    LikedProduct,
+    ProductRating,
+    CartItem,
+    Address,
+    Order, IPAddress,
+)
+
+
+class LikedProductAdmin(admin.StackedInline):
+    model = LikedProduct
+
+
+class RatingAdmin(admin.StackedInline):
+    model = ProductRating
+
+
+class CartAdmin(admin.StackedInline):
+    model = CartItem
+
+
+class AddressAdmin(admin.StackedInline):
+    model = Address
+
+
+UserAdmin.fieldsets[2][1]['fields'] = (
+    'is_active',
+    'is_staff',
+    'is_superuser',
+    'groups',
+    'user_permissions'
+)
+
+UserAdmin.fieldsets[3][1]['fields'] = (
+    'last_login',
+    'date_joined',
+)
+
+admin.site.register(
+    User,
+    UserAdmin,
+    inlines=[LikedProductAdmin, RatingAdmin, CartAdmin, AddressAdmin]
+)
+
+admin.site.register(
+    Order,
+    inlines=[CartAdmin]
+)
+admin.site.register(ProductRating)
+admin.site.register(IPAddress)
