c751a66619475faac9c943d6b05ce259f8f02192
AlirezaAbavi <abavi.alireza2002@gmail.com>
2023-01-06 22:07:40 +0330

    add SaveIPAddress Middleware in app_store

--- ecommerce_api/settings.py
+++ ecommerce_api/settings.py
@@ -60,6 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'app_store.middleware.SaveIPAddressMiddleware',
 ]
 
 REST_FRAMEWORK = {
