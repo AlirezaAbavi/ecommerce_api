2a97067824998f44e7de1113acc91889cc42c223
AlirezaAbavi <abavi.alireza2002@gmail.com>
2023-01-07 18:27:51 +0330

    modify urls

--- ecommerce_api/urls.py
+++ ecommerce_api/urls.py
@@ -24,7 +24,7 @@ from django.conf import settings
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="e-commerce API",
+        title="E-COMMERCE API",
         default_version='v1',
         description="An e-commerce api developed using django(DRF)",
         terms_of_service="https://www.google.com/policies/terms/",
@@ -43,8 +43,8 @@ urlpatterns = [
     path('api/user/', include('app_account.urls')),
 
     re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 
 ]
 
